1b0e9fdef2e02bd769b3fdecc0adcec903bad273
Leandr0Fr <leandro00fr@gmail.com>
2024-10-31 17:04:02 -0300

    ADD credentials linkedin

--- app/constants/constants.py
+++ app/constants/constants.py
@@ -4,3 +4,7 @@ SECRET_KEY = os.getenv("APIKEY")
 
 _KEYWORDS = os.getenv("KEYWORDS")
 KEYWORDS = _KEYWORDS.split(",")
+
+#Linkedin credentials
+EMAIL = os.getenv("LINKEDIN_EMAIL")
+PSW = os.getenv("LINKEDIN_PSW")
