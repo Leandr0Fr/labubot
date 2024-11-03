d04dd0993748147e5a8bed24e0b68c0e7dac4eca
Leandr0Fr <leandro00fr@gmail.com>
2024-11-03 20:08:52 -0300

    ADD constants

--- app/constants/constants.py
+++ app/constants/constants.py
@@ -1,10 +1,15 @@
 import os
 
+# Gemini
 SECRET_KEY = os.getenv("APIKEY")
 
+# CV
+CV = os.getenv("CV")
+
+# Keywords
 _KEYWORDS = os.getenv("KEYWORDS")
 KEYWORDS = _KEYWORDS.split(",")
 
-#Linkedin credentials
+# Linkedin credentials
 EMAIL = os.getenv("LINKEDIN_EMAIL")
 PSW = os.getenv("LINKEDIN_PSW")
---
+++ app/constants/match_enum.py
@@ -0,0 +1,7 @@
+from enum import Enum
+
+
+class Match_Color(Enum):
+    RED = "ROJO"
+    YELLOW = "AMARILLO"
+    GREEN = "VERDE"
