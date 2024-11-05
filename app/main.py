af05fdb0425024e59e0f9e81fc258d9bbc0d8e8a
Leandr0Fr <leandro00fr@gmail.com>
2024-11-05 19:32:16 -0300

    FIX login and sleep call match_cv

--- app/main.py
+++ app/main.py
@@ -1,17 +1,21 @@
+import time
+
 from dotenv import load_dotenv
 
 from .constants.constants import KEYWORDS
 from .constants.match_enum import Match_Color
 from .gemini.match_prompt_cv import match_cv
-from .scraping.scrap_linkedin import get_jobs
+from .scraping.scrap_linkedin import get_jobs, login
 from .telegram.telegram_bot import send_ofert
 
 
 def main():
     load_dotenv()
+    login()
     for keyword in KEYWORDS:
         jobs = get_jobs(keyword)
         for key, job in jobs.items():
+            time.sleep(1)
             match = match_cv(job)
             color = ""
             if match == Match_Color.AMARILLO:
