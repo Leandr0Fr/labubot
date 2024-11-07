cc4d762e0aacf19a8cb487400632b3df4a52b097
Leandr0Fr <leandro00fr@gmail.com>
2024-11-07 14:04:28 -0300

    REFACTOR send offers

--- app/main.py
+++ app/main.py
@@ -6,26 +6,31 @@ from .constants.constants import KEYWORDS
 from .constants.match_enum import Match_Color
 from .gemini.match_prompt_cv import match_cv
 from .scraping.scrap_linkedin import get_jobs, login
-from .telegram.telegram_bot import send_keyword, send_ofert
+from .telegram.telegram_bot import send_keyword, send_offers
 
 
 def main():
     load_dotenv()
     login()
+    urls_used = []
     for keyword in KEYWORDS:
         jobs = get_jobs(keyword)
-        send_keyword(keyword)
+        offers = ""
         for url, job in jobs.items():
+            if url in urls_used:
+                continue
             time.sleep(1)
             match = match_cv(job)
             color = ""
             if match == Match_Color.AMARILLO:
                 color = "🟡"
-            elif match == Match_Color.VERDE:
+            if match == Match_Color.VERDE:
                 color = "🟢"
-            else:
-                continue
-            send_ofert(url, color)
+            if color:
+                urls_used.append(url)
+                offers += f"Se encontró una oferta {color}: {url}\n"
+        send_keyword(keyword)
+        send_offers(offers)
 
 
 if __name__ == "__main__":
