cc6662e3d47fd1666d7b1e1a6ff0dda9746e18c9
Leandr0Fr <leandro00fr@gmail.com>
2024-11-06 22:42:20 -0300

    ADD retry in send ofert and ADD send_keyword

--- app/telegram/telegram_bot.py
+++ app/telegram/telegram_bot.py
@@ -1,12 +1,24 @@
 import telebot
+from requests.exceptions import ReadTimeout
 
 from ..constants.constants import TELEGRAM_ID, TELEGRAM_KEY
 
+bot = telebot.TeleBot(TELEGRAM_KEY)
+
 
 def send_ofert(url_job: str, color: str) -> None:
-    bot = telebot.TeleBot(TELEGRAM_KEY)
-    bot.send_message(
-        TELEGRAM_ID,
-        f"""Aquí tienes una oferta interesante {color}
-{url_job}""",
-    )
+    try:
+        bot.send_message(
+            TELEGRAM_ID,
+            f"""Aquí tienes una oferta interesante {color} 
+            {url_job}""",
+        )
+    except ReadTimeout:
+        send_ofert(url_job, color)
+
+
+def send_keyword(keyword: str) -> None:
+    try:
+        bot.send_message(TELEGRAM_ID, f"Ofertas de {keyword}")
+    except ReadTimeout:
+        send_keyword(keyword)
