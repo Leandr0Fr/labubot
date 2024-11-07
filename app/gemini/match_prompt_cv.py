af8657ddb87f62907ae2b3f1a58d960e10565043
Leandr0Fr <leandro00fr@gmail.com>
2024-11-06 22:41:20 -0300

    REFORMAT prompt match cv

--- app/gemini/match_prompt_cv.py
+++ app/gemini/match_prompt_cv.py
@@ -8,13 +8,30 @@ model = genai.GenerativeModel("gemini-1.5-flash")
 
 
 def match_cv(job: str) -> enumerate:
-    response = model.generate_content(f"""
-    Compare la siguiente oferta laboral: {job}
-    con el siguiente CV: {CV}
-    Descarta las ofertas para seniors, marcalas como "RED".
-    Si es compatible escribe 'GREEN'.
-    Si no tan compatible (capaz por año de experiencia o tecnologías faltantes) escribe 'YELLOW'.
-    Si no es compatible escribe 'RED'.
-    SOLAMENTE ESCRIBE EL COLOR QUE TE PAREZCA, NADA MÁS NI NADA MENOS.
-    """)
-    return Match_Color(response.text.strip())
+    try:
+        response = model.generate_content(f"""
+        Vas a revisar una oferta laboral y un CV.
+        Vas a decidir si el CV es compatible con la oferta laboral.
+        pero vas a tener las siguientes condiciones:
+        1. Descarta las ofertas para seniors.
+        2. Descarta las ofertas que están escritas en inglés.
+        3. Descarta las ofertas que están en blanco.
+        4. Escribiras "GREEN" sí y solo sí, la oferta sea muy compatible con el
+        CV, es decir, la experiencia solicitada sea razonable y 
+        las tecnologias pedidas coincidan en su mayoria.
+        5. Escribiras "YELLOW" sí y solo sí, la oferta no sea tan compatible con
+        con el CV, es decir, no hay tantas tecnologías pedidas que coincidan,
+        se pide más de 2 años de experiencia y se pide un nivel de inglés superior
+        al del CV.
+        6. Escribiras 'RED' sí y solo sí, la oferta no es compatible, es decir, que
+        se solicitan tecnologías muy distintas o muchos años de experiencia. También si
+        cumple algunas de las siguientes coindiciones: 1, 2 o 3.
+                                          
+        SOLAMENTE ESCRIBE EL COLOR QUE DEFINISTE CUANDO LO COMPARES, NADA MÁS NI NADA MENOS.
+                                    
+        Oferta laboral: {job}
+        CV: {CV}
+        """)
+        return Match_Color(response.text.strip())
+    except Exception:
+        match_cv(job)
